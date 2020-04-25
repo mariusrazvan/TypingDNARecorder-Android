403431155d3b0fb9c900dfa5d56764fcee6216dc
Varvarei Marius Razvan <mariusrazvan@protonmail.com>
2020-04-26 02:50:00 +0300

    Update TypingDNARecorderMobile.java

--- typingdnarecordermobile/src/main/java/com/typingdna/TypingDNARecorderMobile.java
+++ typingdnarecordermobile/src/main/java/com/typingdna/TypingDNARecorderMobile.java
@@ -27,35 +27,6 @@
  * tdna.reset(); // restarts the recording anytime, clears history stack and starts from scratch (returns nothing)
  */
 
-/**
- * TypingDNA - Typing Biometrics Recorder for Android
- * https://www.typingdna.com
- *
- * @version 3.1
- * @author Raul Popa & Stefan Endres
- * @copyright TypingDNA Inc. https://www.typingdna.com
- * @license http://www.apache.org/licenses/LICENSE-2.0
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * <p>
- * Typical usage:
- * TypingDNARecorderMobile tdna = new TypingDNARecorderMobile(this); // creates a new TypingDNA object and starts recording
- * String typingPattern = tdna.getTypingPattern(type, length, text, textId, targetId, caseSensitive);
- * <p>
- * Optional:
- * tdna.stop(); // ends recording and clears history stack (returns recording flag: false)
- * tdna.start(); // restarts the recording after a stop (returns recording flag: true)
- * tdna.reset(); // restarts the recording anytime, clears history stack and starts from scratch (returns nothing)
- */
-
 package com.typingdna;
 
 import android.app.Activity;
